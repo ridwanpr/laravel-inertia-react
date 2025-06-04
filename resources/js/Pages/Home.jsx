d042ef8c9a3cb62049f94f9cd41e0d168092b533
Ridwan <ridwanpr@outlook.com>
2025-06-05 03:26:58 +0700

    first commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,49 @@
+import { Link } from "@inertiajs/react";
+
+const Home = ({ posts }) => {
+    return (
+        <>
+            <h1 className="title">Hello</h1>
+
+            <div>
+                {posts.data.map((post) => (
+                    <div key={post.id} className="p-4 border-b">
+                        <div className="text-sm text-slate-600">
+                            <span>Posted on: </span>
+                            <span>
+                                {new Date(post.created_at).toTimeString()}
+                            </span>
+                        </div>
+                        <p className="font-medium">{post.body}</p>
+                    </div>
+                ))}
+            </div>
+
+            <div className="py-12 px-4">
+                {posts.links.map((link, index) =>
+                    link.url ? (
+                        <Link
+                            key={index}
+                            href={link.url}
+                            dangerouslySetInnerHTML={{ __html: link.label }}
+                            className={`px-4 py-2 mx-1 bg-slate-600 text-white rounded-md ${
+                                link.active ? "bg-slate-800" : ""
+                            }`}
+                            preserveScroll
+                        />
+                    ) : (
+                        <span
+                            key={index}
+                            dangerouslySetInnerHTML={{ __html: link.label }}
+                            className="px-4 py-2 mx-1 bg-gray-400 text-white rounded-md"
+                        />
+                    )
+                )}
+            </div>
+        </>
+    );
+};
+
+Home.ayout = (page) => <Layout children={page} />;
+
+export default Home;
